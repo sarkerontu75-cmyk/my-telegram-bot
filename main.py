454209b94dcfae60c17b79c8ae761cfdfab0208a
sarkerontu75-cmyk <sarkerontu75@gmail.com>
2026-02-26 18:25:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler
 
